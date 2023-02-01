287a1fd743926aac43cea284f7efe49d872e8310
Loren Mariquit <l.mariquit@gmail.com>
2023-01-31 23:41:31 -0500

    adjust for very small screens + remove caption

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Caption from "./Caption.js"
 import "../styles/Scoreboard.scss"
 
 const ScoreBoard = ({
@@ -17,20 +16,19 @@ const ScoreBoard = ({
 
   return (
     <div className="scoreboard-wrapper">
-      <Caption winner={winner} turnCount={turnCount} />
-      <div className="scoreboard">
+      <div className={`scoreboard ${winner ? 'winner-' + winner : ''}`}>
         <div className="scoreboard__game-info">
           <div className="scoreboard__game-title">Game</div>
           <div className="scoreboard__game-count">{gameNumber}</div>
         </div>
         <div className={`scoreboard__team x ${activePlayer === "X" && !winner && turnCount < 9 ? "active" : ""}`}>
-          <div className="scoreboard__turn-status">Your Move</div>
+          <div className="scoreboard__turn-status">{winner ? "WINNER!" : "Your Move"}</div>
           <span className="scoreboard__teamname">X</span>
           <span className="scoreboard__wins">{scoreX}</span>
         </div>
 
         <div className={`scoreboard__team o ${activePlayer === "O"  && !winner && turnCount < 9 ? "active" : ""}`}>
-          <div className="scoreboard__turn-status">Your Move</div>
+          <div className="scoreboard__turn-status">{winner ? "WINNER!" : "Your Move"}</div>
           <span className="scoreboard__wins">{scoreO}</span>
           <span className="scoreboard__teamname">O</span>
         </div>
